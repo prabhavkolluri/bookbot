9886d83ac415f8c59768c28e674fba02683b3aa2
prabhavkolluri <prabhavkolluri@gmail.com>
2025-08-26 16:26:21 +0000

    added functionality to choose books from available files.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words,get_letter_count,sorted_dict_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -6,18 +7,26 @@ def get_book_text(filepath):
     return book_text
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
 
-    num_words = get_num_words(text)
-    letter_count = get_letter_count(text)
-    sorted_letter_list = sorted_dict_list(letter_count)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for letter_dict in sorted_letter_list:
-        print(f"{letter_dict["char"]}: {letter_dict["num"]}")
-    print("============= END ===============")
+    # Exit program if program command is executed incorrectly
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    # Run as normal if command is executed correctly
+    else:
+        text = get_book_text(sys.argv[1])
+
+        num_words = get_num_words(text)
+        letter_count = get_letter_count(text)
+        sorted_letter_list = sorted_dict_list(letter_count)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for letter_dict in sorted_letter_list:
+            print(f"{letter_dict["char"]}: {letter_dict["num"]}")
+        print("============= END ===============")
 
 main()
